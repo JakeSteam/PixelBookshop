ad90eb78e65ccb43f8e6679383cf2f298fc6d2de
Jake Lee <github@jakelee.co.uk>
2020-01-19 22:32:51 +0000

    Adds furniture and all metadata

---
+++ app/src/main/java/uk/co/jakelee/pixelbookshop/data/Furniture.kt
@@ -0,0 +1,35 @@
+package uk.co.jakelee.pixelbookshop.data
+
+import androidx.annotation.StringRes
+import uk.co.jakelee.pixelbookshop.R
+
+enum class Furniture(
+    type: FurnitureType, id: Int, tier: Int, @StringRes name: Int, cost: Int,
+    capacity: Int, // Decoration: Range. Display: Books shown. Seating: Seats. Storage: Books stored.
+    level: Int, // Level required to purchase
+    rarity: BookRarity // Decoration: N/A. Display: Max rarity shown. Seating: Max rarity read there. Storage: Books extra capacity applies too.
+) {
+    SingleCandle(FurnitureType.Decoration, 1001, 1, R.string.decoration_singlecandle,
+        100, 1, 1, BookRarity.Common),
+    TripleCandle(FurnitureType.Decoration, 1002, 2, R.string.decoration_triplecandle,
+        200, 2, 10, BookRarity.Common),
+
+    Lectern(FurnitureType.Display, 2001, 1, R.string.display_lectern,
+        100, 10, 1, BookRarity.Common),
+    Shelf(FurnitureType.Display, 2002, 2, R.string.display_shelf,
+        200, 20, 5, BookRarity.Uncommon),
+    Bookcase(FurnitureType.Display, 2003, 3, R.string.display_bookcase,
+        500, 100, 10, BookRarity.Rare),
+
+    Chair(FurnitureType.Seating, 3001, 1, R.string.seating_chair,
+        100, 1, 1, BookRarity.Common),
+    RoundTable(FurnitureType.Seating, 3002, 2, R.string.seating_roundtable,
+        500, 4, 5, BookRarity.Rare),
+
+    SmallCrate(FurnitureType.Storage, 4001, 1, R.string.storage_smallcrate,
+        100, 10, 1, BookRarity.Common),
+    BigCrate(FurnitureType.Storage, 4002, 2, R.string.storage_bigcrate,
+        500, 30, 1, BookRarity.Uncommon),
+
+
+}
---
+++ app/src/main/java/uk/co/jakelee/pixelbookshop/data/FurnitureType.kt
@@ -0,0 +1,11 @@
+package uk.co.jakelee.pixelbookshop.data
+
+import androidx.annotation.StringRes
+import uk.co.jakelee.pixelbookshop.R
+
+enum class FurnitureType(id: Int, @StringRes name: Int) {
+    Decoration(1, R.string.furniture_decoration),
+    Display(2, R.string.furniture_display),
+    Seating(3, R.string.furniture_seating),
+    Storage(4, R.string.furniture_storage)
+}
