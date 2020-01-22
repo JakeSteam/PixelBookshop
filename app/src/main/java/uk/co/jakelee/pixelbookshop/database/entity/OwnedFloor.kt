e09d87777a0b37d0b88cec29cc437f0cc0a2eb30
Jake Lee <github@jakelee.co.uk>
2020-01-22 21:55:18 +0000

    Simplifies layout code & click areas. Changes owned floor primary key

--- app/src/main/java/uk/co/jakelee/pixelbookshop/database/entity/OwnedFloor.kt
+++ app/src/main/java/uk/co/jakelee/pixelbookshop/database/entity/OwnedFloor.kt
@@ -2,11 +2,9 @@ package uk.co.jakelee.pixelbookshop.database.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 
-@Entity
+@Entity(primaryKeys = ["x", "y"])
 data class OwnedFloor(
-    @PrimaryKey val id: Int,
     @ColumnInfo val x: Int,
     @ColumnInfo val y: Int,
     @ColumnInfo val exists: Boolean
--- app/src/main/java/uk/co/jakelee/pixelbookshop/ui/shop/ShopFragment.kt
+++ app/src/main/java/uk/co/jakelee/pixelbookshop/ui/shop/ShopFragment.kt
@@ -1,7 +1,11 @@
 package uk.co.jakelee.pixelbookshop.ui.shop
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -33,60 +37,89 @@ class ShopFragment : Fragment() {
         super.onResume()
         floor_layer.removeAllViews()
 
-        // Floor
-        val floors = getFloor()
+        // Y = left to top
+        // X = left to bottom
 
-        // Sizes used for padding calculations
-        val tileHeight = 128
-        val tileWidth = 256
-        val yTiles = floors.size
+        val floors = getFloor()
         val xTiles = floors.first().size
 
         for (floor in floors) {
             for (tile in floor) {
-                val layoutParams = RelativeLayout.LayoutParams(256, 512) // Sizes used for size calculations
-                val leftPadding = (tile.x + tile.y) * (tileWidth / 2)
-                val topPadding = (tile.y + xTiles - tile.x) * (tileHeight / 2)
-                layoutParams.setMargins(leftPadding, topPadding, 0, 0)
-
-                val image = ImageView(activity!!).apply {
-                    if (tile.exists) setImageResource(R.drawable.floor_planks)
-                    setOnClickListener {
-                        Toast.makeText(
-                            activity,
-                            "Clicked tile (${tile.x},${tile.y})!",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-                floor_layer.addView(image, layoutParams)
+                floor_layer.addView(createTile(tile), getTileParams(tile.x, tile.y, xTiles))
             }
         }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun createTile(tile: OwnedFloor): ImageView {
+        val image = ImageView(activity!!)
+        if (tile.exists) image.setImageResource(R.drawable.floor_planks)
+
+        image.isDrawingCacheEnabled = true
+        image.setOnTouchListener(View.OnTouchListener { v, event ->
+            val bmp = Bitmap.createBitmap(v.drawingCache)
+            if (event.x.toInt() > bmp.width || event.y.toInt() > bmp.height) return@OnTouchListener true
+            val color = bmp.getPixel(event.x.toInt(), event.y.toInt())
+            if (color == Color.TRANSPARENT) {
+                return@OnTouchListener false
+            } else {
+                if (event.action == MotionEvent.ACTION_UP) {
+                    Toast.makeText(activity, "Clicked tile (${tile.x},${tile.y})!", Toast.LENGTH_SHORT).show()
+                }
+                return@OnTouchListener true
+            }
+        })
+        return image
+    }
+
+    private fun getTileParams(x: Int, y: Int, xTiles: Int): RelativeLayout.LayoutParams {
+        val tileWidth = 64
+        val tileHeight = 128
+        val layoutParams = RelativeLayout.LayoutParams(tileWidth, tileHeight)
+        val leftPadding = (x + y) * (tileWidth / 2)
+        val topPadding = (xTiles + (y - x)) * (tileHeight / 8)
+        layoutParams.setMargins(leftPadding, topPadding, 0, 0)
+        return layoutParams
     }
 
     // List[Y][X]
     private fun getFloor(): List<List<OwnedFloor>> {
         return listOf(
             listOf(
-                OwnedFloor(0, 3, 0, true),
-                OwnedFloor(0, 2, 0, true),
-                OwnedFloor(0, 1, 0, true),
-                OwnedFloor(0, 0, 0, true)
+                OwnedFloor(3, 0, true),
+                OwnedFloor(2, 0, true),
+                OwnedFloor(1, 0, true),
+                OwnedFloor(0, 0, true)
             ), listOf(
-                OwnedFloor(0, 3, 1, true),
-                OwnedFloor(0, 2, 1, false),
-                OwnedFloor(0, 1, 1, true),
-                OwnedFloor(0, 0, 1, false)
+                OwnedFloor(3, 1, true),
+                OwnedFloor(2, 1, false),
+                OwnedFloor(1, 1, true),
+                OwnedFloor(0, 1, false)
             ), listOf(
-                OwnedFloor(0, 3, 2, false),
-                OwnedFloor(0, 2, 2, false),
-                OwnedFloor(0, 1, 2, true),
-                OwnedFloor(0, 0, 2, false)
+                OwnedFloor(3, 2, false),
+                OwnedFloor(2, 2, false),
+                OwnedFloor(1, 2, true),
+                OwnedFloor(0, 2, false)
             ), listOf(
-                OwnedFloor(0, 3, 3, true),
-                OwnedFloor(0, 2, 3, false),
-                OwnedFloor(0, 1, 3, true),
-                OwnedFloor(0, 0, 3, true)
+                OwnedFloor(3, 3, true),
+                OwnedFloor(2, 3, false),
+                OwnedFloor(1, 3, true),
+                OwnedFloor(0, 3, true)
+            ), listOf(
+                OwnedFloor(3, 4, true),
+                OwnedFloor(2, 4, true),
+                OwnedFloor(1, 4, true),
+                OwnedFloor(0, 4, true)
+            ), listOf(
+                OwnedFloor(3, 5, true),
+                OwnedFloor(2, 5, true),
+                OwnedFloor(1, 5, true),
+                OwnedFloor(0, 5, true)
+            ), listOf(
+                OwnedFloor(3, 6, true),
+                OwnedFloor(2, 6, true),
+                OwnedFloor(1, 6, true),
+                OwnedFloor(0, 6, true)
             )
         )
     }
